929775ec19fdbd822fe37d7b0912280438e81ad9
HR0530 <roman053016@gmail.com>
2025-04-26 11:02:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 360;
-canvas.height = 640;
+canvas.width = 300;
+canvas.height = 500;
 
 let isGameStarted = false;
 let isGameOver = false;
